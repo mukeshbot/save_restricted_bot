cfe91de272985f539a520398b62ae7a879fd92d1
mukeshbot <145697365+mukeshbot@users.noreply.github.com>
2023-09-21 22:10:48 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -18,12 +18,12 @@ async def sett(event):
         xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
         x = await conv.get_reply()
         if not x.media:
-            xx.edit("No media found.")
+            xx.edit("No media found🥺")
         mime = x.file.mime_type
         if 'png' not in mime and 'jpg' not in mime and 'jpeg' not in mime:
-            return await xx.edit("No image found.")
+            return await xx.edit("No image found😔")
         await xx.delete()
-        t = await event.client.send_message(event.chat_id, 'Trying.')
+        t = await event.client.send_message(event.chat_id, 'Wait....')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
@@ -36,9 +36,9 @@ async def remt(event):
     await event.edit('Trying.')
     try:
         os.remove(f'{event.sender_id}.jpg')
-        await event.edit('Removed😡')
+        await event.edit('Removed🥺')
     except Exception:
-        await event.edit("No thumbnail saved🥺.")                        
+        await event.edit("No thumbnail saved😡")                        
   
 @Invix.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
@@ -54,8 +54,8 @@ async def start(event):
     
     await event.reply(text, 
                       buttons=[
-                              [Button.inline("SET THUMB.", data="set"),
-                               Button.inline("REM THUMB.", data="rem")],
-                              [Button.url("⭐ Mukesh kumawat ⭐", url="https://telegram.dog/mukesh_k_017")]])
+                              [Button.inline("📍SET THUMB📍", data="set"),
+                               Button.inline("📍REM THUMB📍", data="rem")],
+                              [Button.url("🔥 Mukesh kumawat 🔥", url="https://telegram.dog/mukesh_k_017")]])
     
     
